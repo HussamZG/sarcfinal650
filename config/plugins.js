bc8f2773ea01d077d8de580219ed78468b7c3df7
Hussam-Shtayer <108407575+HussamZG@users.noreply.github.com>
2024-11-02 18:15:42 +0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,8 @@
-module.exports = {
-
-};
+module.exports = ({ env }) => ({
+    // إعدادات أخرى
+    'users-permissions': {
+        config: {
+            jwtSecret: env('JWT_SECRET', 'B9VzNnpSrksJD9Sq8MfX8g==')
+        }
+    }
+});
